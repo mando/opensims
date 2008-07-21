108c6584cea8d5fcec2441671b2714c8ee3cb2d2
Mando Escamilla <mando@mando.org>
2008-07-21 13:55:19 -0500

    removed teh sleeping.

--- lib/scripts/importer.rb
+++ lib/scripts/importer.rb
@@ -10,14 +10,17 @@ File.open("data/alerts").each do |line|
     rule     = SnortRule.find_or_create_by_description( :description => desc )
      
     a = Alert.create(
-      :source_host_id => source.id,
-      :source_host_port => src_port,
-      :dest_host_port => dest.id,
-      :alert_time => time,
-      :reporting_host_id => reporter.id,
-      :agent_id => agent.id,
-      :snort_rule_id => rule.id
+      :source_host_id     => source.id,
+      :source_host_port   => src_port,
+      :dest_host_id       => dest.id,
+      :dest_host_port     => dest_port,
+      :alert_time         => time,
+      :reporting_host_id  => reporter.id,
+      :agent_id           => agent.id,
+      :snort_rule_id      => rule.id
     )
+    puts "Sleeping"
+    sleep 10
     puts "TIME: #{time} DESC: #{desc} TRANSPORT: #{transport} SRC_IP: #{src_ip} SRC_PORT: #{src_port} DEST_IP: #{dest_ip} DEST_PORT: #{dest_port}"
   end
 end 
