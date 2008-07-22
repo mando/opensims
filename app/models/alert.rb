30f68b0a2214a2a60661a0fd46101ddf36732a32
Mando Escamilla <mando@mando.org>
2008-07-21 20:54:30 -0500

    Added better relationships to alert.

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,2 +1,4 @@
 class Alert < ActiveRecord::Base
+  belongs_to :source_host, :class_name => "Host", :foreign_key => 'source_host_id'
+  belongs_to :destination_host, :class_name => "Host", :foreign_key => 'dest_host_id'
 end
